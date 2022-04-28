444d525ccfd9ec1f288dd5a2e7cb6a70aab96902
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-04-29 00:23:51 +0530

    Get task from user

--- app/src/main/java/io/tasks/AddTask.kt
+++ app/src/main/java/io/tasks/AddTask.kt
@@ -2,25 +2,37 @@ package io.tasks
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddTask : AppCompatActivity() {
-    private lateinit var completeButton: FloatingActionButton
+    private lateinit var taskText: EditText
+    private lateinit var completeButton: ExtendedFloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
 
         completeButton = findViewById(R.id.complete)
+        taskText = findViewById(R.id.taskEditor)
         completeButton.setOnClickListener(::add)
     }
 
     private fun add(view: View) {
+        val task = taskText.text.toString().trim()
+        if (task.isEmpty()) {
+            taskText.setHintTextColor(resources.getColor(R.color.red, null))
+            return
+        }
         val intent = Intent()
-        intent.putExtra("action", "Testing")
+        intent.putExtra("action", task)
         setResult(Activity.RESULT_OK, intent)
         finish()
     }
