74081e7abaaa566acce98a9f881af78257b3ff26
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-04-28 22:55:47 +0530

    Add floating action buttion

--- app/src/main/java/io/tasks/MainActivity.kt
+++ app/src/main/java/io/tasks/MainActivity.kt
@@ -5,9 +5,10 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var addButton: Button
+    private lateinit var addButton: FloatingActionButton
     private lateinit var tasksRecyclerView: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
     private val tasks = arrayListOf<Task>()
