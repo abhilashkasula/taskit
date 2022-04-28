0f6d11108d49f2c9a562e0a23c7141c44c4ad0a7
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-04-29 00:53:51 +0530

    Add cancel button

--- app/src/main/java/io/tasks/AddTask.kt
+++ app/src/main/java/io/tasks/AddTask.kt
@@ -15,17 +15,25 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class AddTask : AppCompatActivity() {
     private lateinit var taskText: EditText
     private lateinit var completeButton: ExtendedFloatingActionButton
+    private lateinit var cancelButton: ExtendedFloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
 
         completeButton = findViewById(R.id.complete)
+        cancelButton = findViewById(R.id.cancel)
         taskText = findViewById(R.id.taskEditor)
         completeButton.setOnClickListener(::add)
+        cancelButton.setOnClickListener(::cancel)
     }
 
-    private fun add(view: View) {
+    private fun cancel(view: View?) {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    private fun add(view: View?) {
         val task = taskText.text.toString().trim()
         if (task.isEmpty()) {
             taskText.setHintTextColor(resources.getColor(R.color.red, null))
