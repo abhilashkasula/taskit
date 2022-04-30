0447e8447daa3435e7df507e33583592d3f2675e
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-04-30 18:56:31 +0530

    Add progress

--- app/src/main/java/io/taskIt/MainActivity.kt
+++ app/src/main/java/io/taskIt/MainActivity.kt
@@ -3,6 +3,7 @@ package io.taskIt
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -11,21 +12,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.snackbar.Snackbar
 import io.taskIt.db.TasksDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var addButton: FloatingActionButton
     private lateinit var tasksRecyclerView: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
     private lateinit var emptyTasks: LinearLayout
+    private lateinit var tasksProgress: LinearProgressIndicator
     private val tasks = arrayListOf<Task>()
     private val db = TasksDatabase(this)
-
-    private var currentId: Int = 0
 
     private val getContent: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
@@ -43,13 +45,22 @@ class MainActivity : AppCompatActivity() {
         tasks.add(0, task)
         tasksAdapter.notifyItemInserted(0)
         db.addTask(task)
-        currentId++
+
+        updateProgress()
+    }
+
+    private fun updateProgress() {
+        val tasksCompleted = tasks.filter { it.isCompleted }.size
+        val total = tasks.size
+        val progress = (tasksCompleted / total.toFloat()) * 100
+        tasksProgress.progress = progress.toInt()
     }
 
     private fun onTaskCompletionStatusChanged(position: Int, isCompleted: Boolean) {
         val task = tasks[position]
         task.isCompleted = isCompleted
         db.updateTask(task)
+        updateProgress()
         if (!tasksRecyclerView.isComputingLayout) {
             tasksAdapter.notifyItemChanged(position)
         }
@@ -60,12 +71,14 @@ class MainActivity : AppCompatActivity() {
         tasks.remove(task)
         task.id?.let { db.removeTask(it) }
         tasksAdapter.notifyItemRemoved(position)
+        updateProgress()
 
         Snackbar.make(tasksRecyclerView, "The task was deleted", Snackbar.LENGTH_LONG)
             .setAction("Undo") {
                 tasks.add(position, task)
                 db.addTask(task)
                 tasksAdapter.notifyItemInserted(position)
+                updateProgress()
             }.show()
     }
 
@@ -76,6 +89,7 @@ class MainActivity : AppCompatActivity() {
         addButton = findViewById(R.id.add)
         tasksRecyclerView = findViewById(R.id.tasks)
         emptyTasks = findViewById(R.id.emptyTasks)
+        tasksProgress = findViewById(R.id.tasksProgress)
         tasksAdapter = TasksAdapter(this, tasks, ::onTaskCompletionStatusChanged)
         tasksAdapter.registerAdapterDataObserver(DataChangeObserver(tasksRecyclerView, emptyTasks, tasksAdapter))
         tasksRecyclerView.adapter = tasksAdapter
@@ -94,6 +108,9 @@ class MainActivity : AppCompatActivity() {
             val tasksLoadedFromDB = db.getTasks()
             tasksLoadedFromDB.forEach { tasks.add(it) }
             tasksAdapter.notifyDataSetChanged()
+            withContext(Dispatchers.Main.immediate) {
+                updateProgress()
+            }
         }
     }
 
